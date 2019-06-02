56afa79d326842aa97672ed74afdd569161d11ce
sistoabaco <sistodacruzabaco@hotmail.com>
2019-06-02 17:16:58 +0200

    joinning parts2

--- functions/Conexao.php
+++ functions/Conexao.php
@@ -3,11 +3,11 @@
 		
 		private $connection;
 
-        private $URL = "localhost";
-        private $USER ="root";
-        private $DB ="pautas_fe";
-        private $PASSWORD="dblinkx";
-        private $PORT ="3307";
+        private $URL = "jsftj8ez0cevjz8v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        private $USER ="ufm6xc4c1m8wcg8b";
+        private $DB ="ja75c6n0soets4l4";
+        private $PASSWORD="g3ellqs2nhznnd7b";
+        private $PORT ="3306";
 
 		public function __construct(){
 			$this->openConection();
