4a66f7fc873631cab2dc0be5692f6a79d1173be2
Biemos <fbiemo@unilurio.ac.mz>
2019-07-03 13:26:12 +0200

    primeiro lanacamento do poroj

--- tests/vendor/aiw/phpunit-tool/tests/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-defined('TEST_DB_SERVER') || define('TEST_DB_SERVER', '');
-defined('TEST_DB_NAME') || define('TEST_DB_NAME', '');
-defined('TEST_DB_USERNAME') || define('TEST_DB_USERNAME', '');
-defined('TEST_DB_PASSWORD') || define('TEST_DB_PASSWORD', '');
-defined('DOCUMENT_ROOT') || define('DOCUMENT_ROOT', '');
