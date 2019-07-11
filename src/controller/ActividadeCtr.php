70f67659cd2c8567363e5995bafc2f9f46cd1c53
fBiemo <50956964+fBiemo@users.noreply.github.com>
2019-07-11 12:03:18 +0200

    Update ActividadeCtr.php
    adicionando um resrtorno na funcao

--- src/controller/ActividadeCtr.php
+++ src/controller/ActividadeCtr.php
@@ -17,12 +17,13 @@ class MannagerController {
         $result = mysqli_stmt_bind_param($stmt,'s',$descricao);
 
         if(mysqli_stmt_execute($stmt)){
-            echo 'Actividade Criada com sucesso.';
+            $msg = 'Actividade Criada com sucesso.';
         }else{
-            echo 'Encontramos problemas ao Criar a actividade';
+            $msg = 'Encontramos problemas ao Criar a actividade';
         }
 
         $conn->closeDatabase();
+     return $msg;
     }
 
 }
